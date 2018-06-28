847b8587fc8235aaeb22de6c41db8115b9f48bff
Michael Kugler <kuglemic@users.noreply.github.com>
2018-06-28 18:56:03 +0200

    Update ReactNativePermissions.h

--- ReactNativePermissions.h
+++ ReactNativePermissions.h
@@ -7,9 +7,9 @@
 //
 
 #if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
 #import <React/RCTBridgeModule.h>
+#else
+#import "RCTBridgeModule.h"
 #endif
 
 @interface ReactNativePermissions : NSObject <RCTBridgeModule>
